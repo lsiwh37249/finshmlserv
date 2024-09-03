36f332e163b2b8d1e8d353b28e90162696211068
lsiwh37249 <lsiwh37249@gmail.com>
2024-09-03 14:30:39 +0900

    up ver

--- src/finshmlserv/cli.py
+++ src/finshmlserv/cli.py
@@ -16,6 +16,22 @@ def run_prediction(
         l: float = typer.Option(show_default=False),
         w: float = typer.Option(show_default=False)
         ):
+    """주어진 물고기의 길이(l)와 무게(w)를 기반으로 해당 물고기의 종류를 예측하는 함수.
+
+    매개변수:
+    l (float): 물고기의 길이.
+    w (float): 물고기의 무게.
+
+    반환값:
+    str: 예측된 물고기의 종류로, '도미' 또는 '우럭' 중 하나를 반환.
+
+    동작:
+    1. 사전 학습된 모델의 경로를 가져온다.
+    2. 해당 경로에서 모델을 불러온다.
+    3. 불러온 모델을 사용하여 주어진 길이와 무게에 대해 예측을 수행한다.
+    4. 예측 결과에 따라 '도미' 또는 '우럭'을 반환한다.
+    """
+
     path = get_model_path()
     with open(path, "rb") as f:
         fish_model = pickle.load(f)
---
+++ src/finshmlserv/cli_f.py
@@ -0,0 +1,7 @@
+import fire
+
+def prediction_f(l="World"):
+  return "Hello %s!" % l
+
+#if __name__ == '__main__':
+#  fire.Fire(prediction_f)
